12de4243bac03da4b6763132fb10861fac9c9b66
Kyle <kbwillia@gmail.com>
2025-07-07 20:49:26 -0400

    checkpoint before gif dissappaer editing

--- web_app.py
+++ web_app.py
@@ -17,7 +17,7 @@ app.secret_key = 'your-secret-key-here'  # Change this in production
 # Store active games
 games = {}
 
-AI_TURN_DELAY = 1.0  # seconds
+AI_TURN_DELAY = 0.5  # seconds
 
 @app.route('/')
 def index():
